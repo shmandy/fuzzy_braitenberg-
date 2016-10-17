e6ae055f0e84b1b235732a944b1385cbb168ed28
Shmandy <andy@pdx.edu>
2016-10-17 11:11:14 -0700

    + Now compiles...

--- fuzzy_logic/fuzzy_logic.ino
+++ fuzzy_logic/fuzzy_logic.ino
@@ -16,14 +16,15 @@
 #define sensorRight 0
 #define sensorLeft 1
 #define sensorCenter 2
+#define DELAY 1000 // Serial Delay
 
 // Direction Defintions:
 enum DIR
 {
   STOP,
   FORWARD,
-  BACKWORD_SLOW,
-  BACKWORD,
+  BACKWARD_SLOW,
+  BACKWARD,
   TURN_RIGHT,
   TURN_LEFT
 };
@@ -34,6 +35,7 @@ Servo rightServo;
 int leftValue = 0;
 int rightValue = 0;
 int centerValue = 0;
+
 
 DIR currentDirection = STOP;
 DIR nextDirection = STOP;
@@ -61,63 +63,64 @@ void loop() {
     Serial.println("Center: " + centerValue);
     Serial.println("Left: " + leftValue);
     Serial.println("Right: " + rightValue);
+    delay(DELAY);
   #endif
 
-  // Servo Movement Control:
-
-  
-  
-  
-  
-  
+  // Servo Movement Control:  
   if(nextDirection != STOP) // If Stopped:
   {
-    if(!leftSerevo.attached)
+    if(!leftServo.attached())
       leftServo.attach(servoLeft);
-    if(!rightSerevo.attached)
+    if(!rightServo.attached())
       rightServo.attach(servoRight);
   }
   
   // Direction Control
   switch(nextDirection)
   {
-      case: STOP // Stop
+      case STOP: // Stop
       {
         leftServo.detach();
         rightServo.detach(); 
       }
       break;
 
-      case: FORWARD // Forward
+      case FORWARD: // Forward
       {
    
         leftServo.write(140);
         rightServo.write(140);
       }
-
-      case: BACKWARD // Backward Fast
+      break;
+      
+      case BACKWARD: // Backward Fast
       {
          leftServo.write(0);
          rightServo.write(0);
       }
+      break;
       
-      case: BACKWARD_SLOW // Backward Slow
+      case BACKWARD_SLOW: // Backward Slow
       {
          leftServo.write(80);
          rightServo.write(80);
       }
+      break;
 
-      case: TURN_RIGHT   // Turn Right
+      case TURN_RIGHT:   // Turn Right
       {
-         leftServo.write(140);
-         rightServo.detach();
+         leftServo.write(140); // Forward
+         rightServo.write(80); // Backward
       }
+      break;
 
-      case: TURN_LEFT // Turn Left
+      case TURN_LEFT: // Turn Left
       {
-         rightServo.write(140);
-         leftServo.detach();
+         rightServo.write(140); // Forward
+         leftServo.write(80); // Backward
+         
       }
+      break;
       
       default: // Stop
       {
@@ -126,5 +129,8 @@ void loop() {
       }
       break;
   }
+
+  // Simple
+  
 
 }
